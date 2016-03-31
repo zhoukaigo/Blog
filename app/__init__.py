44227df7501e405339f488220c1973d7f20ea094
zhoukaigo <zhoukaig_goforit@foxmail.com>
2016-03-31 20:53:23 +0800

    add user model and flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
-
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
 
 bootstrap = Bootstrap()
+db = SQLAlchemy()
 
-def create_myapp():
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+login_manager.login_message = u'Please log in first!'
+
+def create_app():
 	app = Flask(__name__)
+
 	bootstrap.init_app(app)
+	db.init_app(app)
+	login_manager.init_app(app)
 
 	from .main import main as main_blueprint
 	app.register_blueprint(main_blueprint)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,10 @@
-class Article(db.Model):
-	__tablename__ = 'articles'
+from flask.ext.sqlalchemy import SQLAlchemy 
+from flask.ext.login import LoginManager, UserMixin
+from . import db, login_manager
+
+class User(UserMixin, db.Model):
+	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True)
-	body = db.Column(db.Text)
-	timestamp = db.Column(db.Datetime, index=True, default=datetime.utcnow)
-	
+	social_id = db.Column(db.String(64), nullable=False, unique=True)
+	nickname = db.Column(db.String(64), nullable=False)
+	email = db.Column(db.String(64), nullable=True)
